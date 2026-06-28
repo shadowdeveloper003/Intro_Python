1418905730d9c093b82d278bc053016fb6745c34
Shadow Developer <jothirmoysarker003@gmail.com>
2026-06-28 16:40:03 +0600

    Add list example for card hands
    Added example of a list with card values.

--- test.py
+++ test.py
@@ -19,3 +19,12 @@ inspect(-15)
 x = True
 print(x)
 print(type(x))
+
+#example of list
+hands = [
+    ['J', 'Q', 'K'],
+    ['2', '2', '2'],
+    ['6', 'A', 'K'], # (Comma after the last element is optional)
+]
+# (I could also have written this on one line, but it can get hard to read)
+hands = [['J', 'Q', 'K'], ['2', '2', '2'], ['6', 'A', 'K']]
